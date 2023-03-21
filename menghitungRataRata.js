9de1fd87dbac1d2381c55b6c7944fea8b3720125
arsrsyh <gotrany97@gmail.com>
2023-03-21 14:22:06 +0800

    commit

--- menghitungRataRata.js
+++ menghitungRataRata.js
@@ -1,28 +1,31 @@
-let bahasaIndonesia ='';
-let bahasaInggris = 80;
-let matematika = 90;
-let ipa = 75;
-let hasil ;
-let grade = '';
-
-if (bahasaIndonesia == false || bahasaInggris == false || matematika == false || ipa == false) {
-    console.log('Nilai harus di input');
-} else if (typeof bahasaIndonesia != 'number' || typeof bahasaInggris != 'number' || typeof matematika != 'number' ||typeof ipa != 'number') {
-    console.log('Nilai harus angka');
-} else if (bahasaIndonesia <= 9 || bahasaInggris <= 9 || matematika <= 9 || ipa <= 9) {
-    console.log('Nilai harus lebih besar dari 10');
-} else {
-    hasil = (bahasaIndonesia + bahasaInggris + matematika + ipa) / 4;
-  if (hasil >= 90) {
-    grade = "A";
-  } else if (hasil >= 80) {
-    grade = "B";
-  } else if (hasil >= 70) {
-    grade = "C";
-  } else if (hasil >= 60) {
-    grade = "D";
+const menghitungNilai = function (bi, bingrs, mtk, ipa, hasil, grade) {
+  
+  if (bi == false || bingrs == false || mtk == false || ipa == false) {
+    console.log("Nilai harus di input");
+  } else if (
+    typeof bi != "number" ||
+    typeof bingrs != "number" ||
+    typeof mtk != "number" ||
+    typeof ipa != "number"
+  ) {
+    console.log("Nilai harus angka");
+  } else if (bi <= 9 || bingrs <= 9 || mtk <= 9 || ipa <= 9) {
+    console.log("Nilai harus lebih besar dari 10");
   } else {
-    grade = "E";
+    hasil = (bi + bingrs + mtk + ipa) / 4;
+    if (hasil >= 90) {
+      grade = "A";
+    } else if (hasil >= 80) {
+      grade = "B";
+    } else if (hasil >= 70) {
+      grade = "C";
+    } else if (hasil >= 60) {
+      grade = "D";
+    } else {
+      grade = "E";
+    }
   }
-    console.log(`Nilai Un ${hasil}, grade ${grade}`);
+  console.log(`Nilai Un ${hasil}, grade ${grade}`);
 };
+
+menghitungNilai(90, 90, 80, 85);
